37ee8e0e5a745398deefd648ba9f4b52df578899
Yelyzaveta Zhyrova <lizzzhirova@gmail.com>
2025-09-03 21:25:28 +0300

    DEV-expertssection update responsiveness

--- src/app/components/Templates/ExpertsSection/ExpertsSection.tsx
+++ src/app/components/Templates/ExpertsSection/ExpertsSection.tsx
@@ -3,11 +3,11 @@ import { ExpertCard } from "../../Organisms/ExpertCard/ExpertCard";
 
 export const ExpertsSection = () => {
   return (
-    <section id="masters" className="py-20 max-w-6xl">
+    <section id="masters" className="py-20 max-w-6xl mx-auto">
       <div className="max-w-6xl 
                       mx-auto 
                       px-6
-                      flex flex-col gap-3">
+                      flex flex-col gap-3 md:gap-4">
         <h2 className="text-[32px] md:text-[45px]
                        font-[Manrope-ExtraBold] text-center uppercase 
                        tracking-wider text-[var(--color-brand-gold)]"
@@ -18,12 +18,12 @@ export const ExpertsSection = () => {
         <p className="text-[12px] md:text-[15px]
                       uppercase text-center font-[Manrope-Regular] text-[#CFCFCF]
                       leading-relaxed tracking-[2px]
-                      max-w-2xl mx-auto mb-4">
+                      max-w-2xl mx-auto mb-4 md:mb-7">
           Команда професіоналів Carfix — досвід, який повертає твоє авто в
           ідеальний стан. Знайомтесь з нашими провідними майстрами.
         </p>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 xl:gap-25">
           {experts.map((m, i) => (
             <ExpertCard
               key={i}
